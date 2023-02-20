58d93cdb1fa0e6dbd5e52e191f013b52d2ab8610
Mayank-sudo <mayanloop@gmail.com>
2023-02-20 16:13:21 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import  Herro from './component/Herro'
+
+
+function App() {
+  return (
+    <div className="App">
+    <Herro />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Herro.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+
+import './Hero.css'
+
+const Herro = () => {
+  return (
+    <Container id="coo">
+    <img src="https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/flipkart-plus_8d85f4.png" alt="flip" className='flipo'/>
+   <input type="text" placeholder="Search brand Products and more" id='inpput' />
+     
+    </Container>
+  )
+}
+
+export default Herro
